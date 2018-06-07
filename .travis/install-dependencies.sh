2883bf11cf3fcc80a333bbda6dd8b04ee530bb44
Paxton Hare <paxton@greenllama.com>
2018-06-07 14:37:37 -0400

    fixing copy/paste issues

--- .travis/install-dependencies.sh
+++ .travis/install-dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 if [ "${TRAVIS_SECURE_ENV_VARS}" = "true" ] ; then
-  cd ml-unit-test-client
-  ../gradlew build test
+  ./.travis/travis-install-ml.sh release
+  ./travis/setup-marklogic.sh
 fi
